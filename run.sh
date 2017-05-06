5859a2ae935081e38ef766bedadfede988915573
massimoDC <dallacia@dei.unipd.it>
2017-05-06 16:06:21 +0200

    In the Tfidf.java file there are in the main the istructions to use word2wec. I added also some run.sh file to build all in a short time. Anyway it could be a good idea replace them with a make file

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "inizio compilazione"
+./gradlew compileJava
+echo "fine compilazione"
+./gradlew showDepsClasspath
+export CP=$(./gradlew showDepsClasspath | grep jar)
+java -Dspark.master=local[4] -cp $CP:build/classes/main it.unipd.dei.dm1617.examples.TfIdfTransformation dataset/short.dat
--- src/main/java/it/unipd/dei/dm1617/examples/TfIdfTransformation.java
+++ src/main/java/it/unipd/dei/dm1617/examples/TfIdfTransformation.java
@@ -2,13 +2,23 @@ package it.unipd.dei.dm1617.examples;
 
 import it.unipd.dei.dm1617.*;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.mllib.feature.IDF;
+import org.apache.spark.mllib.feature.Word2VecModel;
 import org.apache.spark.mllib.linalg.Vector;
 import scala.Tuple2;
+import org.apache.spark.mllib.feature.Word2Vec;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.SparkSession;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,10 +56,7 @@ public class TfIdfTransformation {
     //
     // In this case we also cache the dataset because the next step,
     // IDF, will perform two passes over it.
-    JavaRDD<Vector> tf = new CountVectorizer()
-      .setVocabularySize(100)
-      .transform(lemmas)
-      .cache();
+    JavaRDD<Vector> tf = new CountVectorizer().setVocabularySize(100).transform(lemmas).cache();
 
     // Same as above, here we follow the same pattern, with a small
     // addition. Some of these "configurable" objects configure their
@@ -69,14 +76,65 @@ public class TfIdfTransformation {
     // therefore the first vector corresponds to the first page and so
     // on.
     JavaPairRDD<WikiPage, Vector> pagesAndVectors = pages.zip(tfidf);
+    List<Tuple2<WikiPage, Vector>> firstPages = pagesAndVectors.take(1000);
 
-    // Finally, we print the distance between the first two pages
-    List<Tuple2<WikiPage, Vector>> firstPages = pagesAndVectors.take(2);
-    double dist = Distance.cosineDistance(firstPages.get(0)._2(), firstPages.get(1)._2());
-    System.out.println("Cosine distance between `" +
-      firstPages.get(0)._1().getTitle() + "` and `" +
-      firstPages.get(1)._1().getTitle() + "` = " + dist);
+    //my adds-----------------------------------------------------------------------
+    Word2Vec w2vec=new Word2Vec();
+    w2vec.setLearningRate(0.025);
+    w2vec.setMaxSentenceLength(5000);
+    w2vec.setMinCount(5);
+    w2vec.setNumIterations(1);
+    w2vec.setNumPartitions(1);
+    w2vec.setVectorSize(100);
+    w2vec.setWindowSize(5);
+    Word2VecModel w2vM=w2vec.fit(lemmas);
+    w2vM.save(JavaSparkContext.toSparkContext(sc),dataPath+".w2v");
 
+
+    System.out.println("################### START ############################\n");
+    //Vector parigi=w2vM.transform("parigi");
+    //Vector roma=w2vM.transform("roma");
+
+    //System.out.println(parigi.toJson());
+
+    System.out.println("\n################### END ############################");
+    //-----------------------------------------------------------------------
+  }
+
+  public static void printCategories(List<Tuple2<WikiPage, Vector>> firstPages){
+    for(int i=0; i<firstPages.size()-1; i++){
+      //print categories of a page
+      //example of a page with no category
+      //if(firstPages.get(i)._1().getTitle().equals("Conductor")){
+        System.out.println(firstPages.get(i)._1().getTitle()+"\n");
+        System.out.println("lunghezza del testo "+firstPages.get(i)._1().getText().length()+"\n");
+        for(String str:firstPages.get(i)._1().getCategories()){
+          System.out.println("\t--> "+str);
+        }
+        System.out.println("--------");
+      //}
+    }
+  }
+
+  public static void print(List<Tuple2<WikiPage, Vector>> firstPages){
+    double dist;
+
+    for(int i=0; i<firstPages.size()-1; i++){
+      if(firstPages.get(i)._1().getCategories().length!=0){
+        for(int j=1; j<firstPages.size(); j++){
+          if(firstPages.get(j)._1().getCategories().length!=0){
+            dist = Distance.cosineDistance(firstPages.get(i)._2(), firstPages.get(j)._2());
+            if(dist>=0.99){
+              printDistance(0,i,dist,firstPages.get(i)._1().getTitle(), firstPages.get(j)._1().getTitle());
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public static void printDistance(int doc1, int doc2, double dist, String tit1, String tit2){
+    System.out.println("Cosine distance between "+tit1+" and "+tit2+" = "+dist);
   }
 
 }
