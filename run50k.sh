1913e4a416f837016523f6600aa8fcde17e03a5f
massimoDC <dallacia@dei.unipd.it>
2017-05-16 21:26:08 +0200

    try kmeans

---
+++ run50k.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "inizio compilazione"
+./gradlew compileJava
+echo "fine compilazione"
+./gradlew showDepsClasspath
+export CP=$(./gradlew showDepsClasspath | grep jar)
+java -Dspark.master=local[4] -Xmx8g -XX:ReservedCodeCacheSize=2g -cp $CP:build/classes/main it.unipd.dei.dm1617.examples.TfIdfTransformation dataset/50k.dat
+
--- src/main/java/it/unipd/dei/dm1617/examples/Cluster.java
+++ src/main/java/it/unipd/dei/dm1617/examples/Cluster.java
@@ -1,8 +1,11 @@
 package it.unipd.dei.dm1617.examples;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import scala.Tuple2;
 import scala.util.parsing.json.JSONObject;
@@ -18,14 +21,28 @@ public class Cluster {
         // Usual setup
         SparkConf conf = new SparkConf(true).setAppName("Tf-Ifd transformation");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("ERROR");
+        System.out.println("##############################################################################################################################");
 
-        //todo come leggo il file?
-        /*//load file
-        JavaRDD<String> lines = sc.textFile(path);
-        JavaRDD<Tuple2<Long, Vector>> wikiVectors = lines.map(line->{
+        //todo cer
+        //load file
+        JavaRDD<Tuple2<Long, Vector>> wikiVectors = sc.objectFile(path);
+        JavaRDD<Vector> prova = wikiVectors.map(elem->{
+            return elem._2();
+        });
+        System.out.println("dimensione di prova "+prova.count());
 
-            return
-        });*/
+        // Cluster the data into two classes using KMeans
+        int numClusters = 60;
+        int numIterations = 100;
+        System.out.println("eseguo il kmeans");
+        KMeansModel clusters = KMeans.train(prova.rdd(), numClusters, numIterations);
+        /*System.out.println("Cluster centers:");
+        for (Vector center: clusters.clusterCenters()) {
+            System.out.println(" " + center);
+        }*/
 
+        double cost = clusters.computeCost(prova.rdd());
+        System.out.println("Cost: " + cost);
     }
 }
