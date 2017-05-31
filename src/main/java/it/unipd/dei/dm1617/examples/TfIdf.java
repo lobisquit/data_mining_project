65c255ba2146a0a258e564b2a04829e0359c66b7
Enrico Vianello <vianello94@gmail.com>
2017-06-01 00:13:04 +0200

    Improved Bag of Words conversion

--- src/main/java/it/unipd/dei/dm1617/examples/TfIdf.java
+++ src/main/java/it/unipd/dei/dm1617/examples/TfIdf.java
@@ -10,13 +10,46 @@ import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.rdd.RDD;
 import scala.Tuple2;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Convert documents in "bag of words" representations using TfIdf
  */
 public class TfIdf {
+
+    private static List<String> readFile(String path) {
+        Scanner s = null;
+        try {
+            s = new Scanner(new File(path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> lines = new ArrayList<>();
+        try {
+            while (s.hasNext()){
+                lines.add(s.next());
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        s.close();
+        return lines;
+    }
+
+    private static void toFile (String filename, String[] x) throws IOException{
+        BufferedWriter outputWriter;
+        outputWriter = new BufferedWriter(new FileWriter(filename));
+        for (int i = 0; i < x.length; i++) {
+            outputWriter.write(x[i]);
+            outputWriter.newLine();
+        }
+        outputWriter.flush();
+        outputWriter.close();
+    }
+
     /**
      * @param args
      *  args[0] : path of the dataset
@@ -25,6 +58,9 @@ public class TfIdf {
     public static void main(String[] args) {
         String dataPath = args[0];
         int vocabularySize = Integer.parseInt(args[1]);
+
+        // Load list of stop words
+        List<String> stopWords = readFile("dataset/stop_words.txt");
 
         // Usual setup
         SparkConf conf = new SparkConf(true).setAppName("Tf-Ifd transformation");
@@ -41,6 +77,18 @@ public class TfIdf {
         // times.
         JavaRDD<ArrayList<String>> lemmas = Lemmatizer.lemmatize(texts).cache();
 
+        // Remove stopwords and short lemmas
+        JavaRDD<ArrayList<String>> purgedLemmas = lemmas.map(array -> {
+            ArrayList<String> newArray = new ArrayList<>();
+            for (String word : array) {
+                // remove words that are too short or that are stop words
+                if (!stopWords.contains(word.toLowerCase()) && word.length() > 2) {
+                    newArray.add(word);
+                }
+            }
+            return newArray;
+        });
+
         // Transform the sequence of lemmas in vectors of counts in a
         // space of 100 dimensions, using the 100 top lemmas as the vocabulary.
         // This invocation follows a common pattern used in Spark components:
@@ -52,10 +100,13 @@ public class TfIdf {
         //
         // In this case we also cache the dataset because the next step,
         // IDF, will perform two passes over it.
-        JavaRDD<Vector> tf = new CountVectorizer()
+        CountVectorizer cv = new CountVectorizer();
+        JavaRDD<Vector> tf = cv
                 .setVocabularySize(vocabularySize)
-                .transform(lemmas)
+                .transform(purgedLemmas)
                 .cache();
+
+        String[] vocabulary = cv.getVocabulary();
 
         // Same as above, here we follow the same pattern, with a small
         // addition. Some of these "configurable" objects configure their
@@ -75,8 +126,15 @@ public class TfIdf {
         // therefore the first vector corresponds to the first page and so
         // on.
         JavaPairRDD<WikiPage, Vector> pagesAndVectors = pages.zip(tfidf);
-        JavaRDD<Tuple2<WikiPage, Vector>> aa = JavaPairRDD.toRDD(pagesAndVectors).toJavaRDD();
-        pagesAndVectors.saveAsObjectFile("output/TfIdf--"+dataPath.split("/")[1]+"--vocabulary:"+vocabularySize+".tfidf");
+        // From PairRDD to RDD
+        JavaRDD<Tuple2<WikiPage, Vector>> pavRDD = JavaPairRDD.toRDD(pagesAndVectors).toJavaRDD();
+
+        try {
+            pavRDD.saveAsObjectFile("output/TfIdf--"+dataPath.split("/")[1]+"--vocabulary:"+vocabularySize+".tfidf");
+            toFile("output/TfIdfVocabulary--"+dataPath.split("/")[1]+"--vocabulary:"+vocabularySize+".txt", vocabulary);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
