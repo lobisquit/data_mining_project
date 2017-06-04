7a8e509d7b9728eeaa82edf9cda682045cf3874c
Davide Cazzaro <davidecazzaro@users.noreply.github.com>
2017-06-04 15:05:41 +0200

    Cleaned up Silhuette code

--- src/main/java/it/unipd/dei/dm1617/examples/SimpleSilhouetteCoefficient.java
+++ src/main/java/it/unipd/dei/dm1617/examples/SimpleSilhouetteCoefficient.java
@@ -25,9 +25,8 @@ public class SimpleSilhouetteCoefficient {
         int kEnd = Integer.parseInt(args[1]);
         int kStep = Integer.parseInt(args[2]);
 
-        //Integer[] listOfModels = {700, 989, 1111, 1571, 1763, 2494, 2800, 3959, 4444, 6285, 7055, 9978, 11200, 15839, 17778, 25143, 28222, 39912, 44800, 63356};
-        // Integer[] listOfModels = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
-        Integer[] listOfModels = {100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199};
+        // note: when listOfModels is set we ignore the command line args passed
+        Integer[] listOfModels = {100, 1000, 10000};
         kStart = 0;
         kEnd = listOfModels.length - 1;
         kStep = 1;
@@ -53,9 +52,9 @@ public class SimpleSilhouetteCoefficient {
 
         for(Integer i= kStart; i <= kEnd; i += kStep){
 
-            System.out.println("Computing kmeans with k="+listOfModels[i].toString()); // i
+            System.out.println("Computing kmeans with k="+listOfModels[i].toString());
 
-            KMeansModel model = getKMeansModel(sc, dataset, clusteringName, listOfModels[i], numIterations); // i
+            KMeansModel model = getKMeansModel(sc, dataset, clusteringName, listOfModels[i], numIterations);
 
             Vector[] centroids = model.clusterCenters();
 
@@ -115,14 +114,14 @@ public class SimpleSilhouetteCoefficient {
 
             Double ssc = sumCoefficients / new Long(simpleSilhoutteCoefficients.count()).doubleValue();
 
+            // not used but useful to compute
             Long totalCount = simpleSilhoutteCoefficients.filter(n -> {
                 return n != 0.0;
             }).count();
-            // Double ssc = sumCoefficients / totalCount.doubleValue();
 
-            results.add(new Tuple2<>(listOfModels[i], ssc)); // i
+            results.add(new Tuple2<>(listOfModels[i], ssc));
 
-            System.out.println("Simple Silhouette Coefficient: " + ssc + " with K="+listOfModels[i].toString()); // i
+            System.out.println("Simple Silhouette Coefficient: " + ssc + " with K="+listOfModels[i].toString());
             System.out.println("Sum: " + sumCoefficients + "totalCount: " + totalCount);
 
         }
